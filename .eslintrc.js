7fc3abbd2aa135962ecb5afbfd109d779d627994
AcrylicShrimp <led789zxpp@naver.com>
2021-08-31 18:35:23 +0900

    chore: configure eslint for svelte.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
   env: {
-    amd: true,
+    es6: true,
     node: true,
     browser: true,
   },
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'svelte3'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'prettier',
+  ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
   ],
   settings: {
     'import/resolver': 'webpack',
